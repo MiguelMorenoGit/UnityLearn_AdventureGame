3cb9bdd72bc3bf673e432d36fa4422770f10a8bf
MiguelMorenoGit <katsura501@gmail.com>
2026-05-22 10:41:30 +0200

    PlayerCharacther created and PLayerController script added

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position = position + new Vector2(0.01f, 0.01f); // Move the player to the right at a constant speed
+        transform.position = position;
     }
 }
