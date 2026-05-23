768e59d49b9352a1f5b401ae51a4ffd07e55dfa8
MiguelMorenoGit <katsura501@gmail.com>
2026-05-24 00:52:54 +0200

    Input Actions added for Keyboard and Gamepad

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem; 
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction; // Input action for moving
+
+    [SerializeField, Range(0f, 0.1f)]
+    public float moveSpeed = 0.05f;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position = position + new Vector2(0.01f, 0.01f); // Move the player to the right at a constant speed
-        transform.position = position;
+        Vector2 move = MoveAction.ReadValue<Vector2>(); // Read the movement input
+        Debug.Log(move);
+
+        // Calculate the new position based on input and move speed
+        Vector2 position = (Vector2)transform.position + move * moveSpeed;
+
+        transform.position = position; // Move the player based on input
+
     }
 }
